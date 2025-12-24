e7d73cfcbb25327073359a022ca47e6c49683f48
AlexB <alexb@example.com>
2025-12-24 17:42:58 +0300

    Updated Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react({
-//       babel: {
-//         plugins: [['babel-plugin-react-compiler']],
-//       },
-//     }),
-//   ],
-// })
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/amazon-clone/" : "/",
+export default defineConfig({
+  base: "/",
 
   plugins: [
     react({
@@ -24,4 +11,4 @@ export default defineConfig(({ mode }) => ({
       },
     }),
   ],
-}));
+});
